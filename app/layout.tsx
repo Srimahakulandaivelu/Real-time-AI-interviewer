dcd99971bc661b9c06dd2a3ead18ccf3273575ad
Mahaa <srimahakulandaivelu05@gmail.com>
2026-07-03 17:28:56 +0530

    Initial project Structure

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import {Mona_Sans, Geist } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PrepWise",
+  description: "An AI-powered platform for preparing for mock interviews",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={cn("dark", "font-sans", geist.variable)}
+    >
+      <body className= {`${monaSans.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
